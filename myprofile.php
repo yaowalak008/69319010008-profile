d93f1b49cc263d9dce5403a1fbf5ffbd091ac8ca
yanisa <yanisabp9@example.com>
2026-07-05 19:35:54 +0000

    comment

---
+++ myprofile.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>My Profile</title>
+</head>
+<body>
+    <h1>Profile: 69319010016 yanisa</h1>
+    <p>Welcome to my page</p>
+    <hr>
+    <h2>Messages from teammates</h2>
+    <div id="friend-messages">
+        <!-- Friends, please add your messages below -->
+69319010008 yaowalak
+    </div>
+</body>
+</html>
